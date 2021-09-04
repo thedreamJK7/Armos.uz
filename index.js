bf5b88d04716258a56421de7bdc6307c7af706fa
thedreamJK7 <jkubaev7@gmail.com>
2021-09-04 22:48:34 +0500

    scroll animation

--- index.js
+++ index.js
@@ -44,3 +44,8 @@ const sr = ScrollReveal({
 sr.reveal(`#second,#third,#fourth,#nineth`,{
     interval: 2000
 });
+
+window.addEventListener('scroll',()=> {
+    let qwer = document.querySelector('header');
+    qwer.classList.toggle('fixed', window.scrollY > 200);
+});
